25290570c9bbae7f0590d8411d52991d5b2c6075
Haidar <akhmadr965@gmail.com>
2020-01-09 09:35:53 +0700

    initial commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\MSI\Flutter SDK\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\MSI\AndroidStudioProjects\project\bloc_hero"
+export "FLUTTER_APPLICATION_PATH=C:\Users\MSI\AndroidStudioProjects\project\Flutter_BLocWithRxdart"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
